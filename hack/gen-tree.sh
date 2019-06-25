6dc453701cde36ba282bfd82af090572bd0e9af3
Holden Karau <holden@pigscanfly.ca>
2019-06-25 09:23:17 -0700

    And gen-tree

--- hack/gen-tree.sh
+++ hack/gen-tree.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $(basename $PWD) != "manifests" ]]; then
+if [[ $(basename $PWD) != "manifests" && ! -d .git ]]; then
   echo "must be at manifests root directory to run $0"
   exit 1
 fi
